5a695d4b13fc6724e7ee4b0c2d45522c16c7399a
Moisés Corrales <moises.corrales.tnf@gmail.com>
2024-11-21 17:59:56 +0100

    IT1: Subir sdk y corregir clases en java

---
+++ app/src/main/java/com/project/atlas/Services/AuthService.kt
@@ -0,0 +1,14 @@
+package com.project.atlas.Services
+
+import com.project.atlas.Interfaces.UserInterface
+
+class AuthService : UserInterface {
+    override fun createUser(eMail: String, pass: String) {
+        // Implementación aquí
+    }
+
+    override fun loginUser(eMail: String, pass: String) {
+        // Implementación aquí
+    }
+}
+
