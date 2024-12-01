7c4faa7cef36c1daaa2a900a85e1dac4e23090e8
DarkPunk <alexbeltransalas@gmail.com>
2024-12-01 20:41:33 +0100

    Map view added + Rute skeleton created

---
+++ app/src/main/java/com/project/atlas/Services/RuteService.kt
@@ -0,0 +1,12 @@
+package com.project.atlas.Services
+
+import com.project.atlas.Models.Location
+import com.project.atlas.Models.Rute
+import com.project.atlas.Models.VehicleModel
+import org.osmdroid.util.GeoPoint
+
+class RuteService {
+    fun calculateRute(start: Location, end: Location, vehicle: VehicleModel): Rute{
+        return Rute()
+    }
+}
