bcb00505202c2c628045cad653d9d2aa9455b3d5
Moisés Corrales <al405196@uji.es>
2024-12-23 22:37:57 +0100

    Corregir electricity-calories

--- app/src/main/java/com/project/atlas/interfaces/EnergyType/Calories.kt
+++ app/src/main/java/com/project/atlas/interfaces/EnergyType/Calories.kt
@@ -3,8 +3,10 @@ import com.project.atlas.interfaces.EnergyType
 class Calories: EnergyType() {
     override val typeName = "Calories"
     override val magnitude = "KCal"
-    override fun calculateCost(distance: Double, consumption: Double, fuelPrice: Double): Double {
-        TODO("Not yet implemented")
+    override fun calculateCost(distance: Double, consumption: Double, price: Double): Double {
+        var velocity = 5
+        if (consumption == 7.0) velocity = 20
+        var result = (distance * consumption * 70) / velocity
+        return result
     }
-
 }
