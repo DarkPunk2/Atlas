6b17f08003c23f7b7b2127dda400faffc2a57135
gui11i <al405775@uji.es>
2025-01-03 01:23:01 +0100

    it5 - solved no vehicles problem

--- app/src/main/java/com/project/atlas/services/VehicleService.kt
+++ app/src/main/java/com/project/atlas/services/VehicleService.kt
@@ -15,6 +15,7 @@ import com.project.atlas.interfaces.VehicleInterface
 import com.project.atlas.models.VehicleModel
 import com.project.atlas.models.VehicleType
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
 
 
 class VehicleService(private val dbService: VehicleDatabaseService) : VehicleInterface {
@@ -83,7 +84,11 @@ class VehicleService(private val dbService: VehicleDatabaseService) : VehicleInt
     }
 
     override fun observeVehicles(user: String): Flow<List<VehicleModel>> {
-        return dbService.observeVehicles(user)
+        return dbService.observeVehicles(user).onEach { vehicleList ->
+            if (vehicleList.isEmpty()) {
+                createDefaults(user)
+            }
+        }
     }
 
     @SuppressLint("SuspiciousIndentation")
