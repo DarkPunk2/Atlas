426a7fd583b2bd8afc960ceb9a3e112693b8c1b2
Moisés Corrales <al405196@uji.es>
2025-01-03 12:24:15 +0100

    H26 integration test

--- app/src/test/java/com/project/atlas/it_4Test/H26DarkLightThemeTest.kt
+++ app/src/test/java/com/project/atlas/it_4Test/H26DarkLightThemeTest.kt
@@ -1,8 +1,29 @@
 package com.project.atlas.it_4Test
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import Calories
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 
 class H26DarkLightThemeTest {
-    
+    val isDarkTheme: MutableState<Boolean> = mutableStateOf(false)
+
+    @Test
+    fun H26P1Test() {
+        val isDarkTheme: MutableState<Boolean> = mutableStateOf(false)
+
+        fun toggleTheme(state: MutableState<Boolean>) {
+            state.value = !state.value
+        }
+
+        assertEquals(false, isDarkTheme.value)
+
+        toggleTheme(isDarkTheme)
+
+        assertEquals(true, isDarkTheme.value)
+
+    }
 
 }
